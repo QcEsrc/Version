566ff45389309d2715843e64903416330ee10158
Soft and pink big breasts <167391904+Cossall@users.noreply.github.com>
2024-07-18 20:29:05 +0800

    Add files via upload

--- app/src/main/java/com/android/activity/ui/task/TaskFragment.kt
+++ app/src/main/java/com/android/activity/ui/task/TaskFragment.kt
@@ -1,1 +1,56 @@
+package com.android.activity.ui.task
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.Button
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.JavascriptInterface
+import android.annotation.SuppressLint
+import android.content.Context
+import android.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import xyn.xyn.xyn.databinding.FragmentTaskBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class TaskFragment : Fragment() {
+
+    private var _binding: FragmentTaskBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val taskViewModel =
+            ViewModelProvider(this).get(TaskViewModel::class.java)
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        
+        
+        
+        return root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

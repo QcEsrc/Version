0d0cfb35176a92df239dd362aad5f76372a8f731
Soft and pink big breasts <167391904+Cossall@users.noreply.github.com>
2024-07-18 20:27:46 +0800

    Add files via upload

---
+++ app/src/main/java/com/android/activity/ui/just/JustViewModel.kt
@@ -0,0 +1,10 @@
+package com.android.activity.ui.just
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class JustViewModel : ViewModel() {
+
+    
+}

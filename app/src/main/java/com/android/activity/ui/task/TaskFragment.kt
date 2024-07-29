6fa6c36ba764b58df2c03c013958ab3a99c93387
Soft and pink big breasts <167391904+Cossall@users.noreply.github.com>
2024-07-29 19:56:56 +0800

    Add files via upload

--- app/src/main/java/com/android/activity/ui/task/TaskFragment.kt
+++ app/src/main/java/com/android/activity/ui/task/TaskFragment.kt
@@ -1,9 +1,14 @@
 package com.android.activity.ui.task
 
 import android.os.Bundle
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import android.graphics.Color
+import android.util.Log
+
 import android.widget.TextView
 import android.widget.EditText
 import android.widget.Button
@@ -12,21 +17,28 @@ import android.webkit.WebViewClient
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.JavascriptInterface
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.app.AlertDialog
+
+import xyn.xyn.xyn.R
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import xyn.xyn.xyn.databinding.FragmentTaskBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
+import java.io.IOException
+import java.io.InputStreamReader
+
 class TaskFragment : Fragment() {
 
     private var _binding: FragmentTaskBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    
     private val binding get() = _binding!!
+    
+    private lateinit var javalogTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,7 +58,9 @@ class TaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       
+        javalogTextView = binding.javalogTextView
+        
+        
     }
 
     override fun onDestroyView() {

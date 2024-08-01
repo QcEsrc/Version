6b49cb46f63a45e6ea252b445c4dbfca0c5115b6
Soft and pink big breasts <167391904+Cossall@users.noreply.github.com>
2024-08-01 17:46:10 +0800

    Update apply.cpp

--- cpp/apply.cpp
+++ cpp/apply.cpp
@@ -1,1 +1,19 @@
-
+#include <system_error>
+#include <cstdlib>
+#include <jni.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map> 
+#include <unordered_set>
+#include <fstream>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <exception>
+#include <android/log.h>
